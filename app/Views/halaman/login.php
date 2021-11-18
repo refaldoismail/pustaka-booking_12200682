2b25cbffbc31262b35718ab34a24a026e3833885
refaldoismail <12200682@bsi.ac.id>
2021-11-18 15:13:00 +0700

    tugas pertemuan 8

--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -1,54 +1,84 @@
-<?=$this->extend('dasar_tampilan');?>
-<?=$this->section('isiweb')?>   
-<form method="POST" action="<?=base_url('/login')?>">
-<div class="container col-md-3">
-    <div class="card mt-5">
-        <div class="card-header">
-            <h3 class="card-title">login</h3>
-        </div>
+<?= $this->extend('dasar_tampilan'); ?>
+<?= $this->section('isiweb') ?>
+<?php
 
-        <div class="card-body">
-        <?php   if($error){ ?>
-                    <div class="alert alert-danger">
-                    <?=$error?>
-                    </div>
-              <?php }  ?>
-            <div class="form-floating">
-                <input id ="txtEmail" class="form-control"
-                    placeholder="Email"
-                    value="<?=$email ?? ''?>"
-                    type="text" name="email">
-                <label for="txtEmail">Email</label>
+use Config\Services;
 
-                <?php   if($vd?->getError('email')){ ?>
-                    <div class="alert alert-danger">
-                    <?php
-                         echo $vd?->getError('email');
-                    ?>
-                    </div>
-              <?php }  ?>
+$vd = $vd ?? Services::validation();
+?>
 
-            </div>
-            <div class="form-floating">
-                <input id="txtPass" class="form-control"
-                    placeholder="Password"
-                    value="<?=$sandi ?? ''?>"
-                    type="password" name="sandi">
-                <label for='txtPass'>Kata Sandi</label>
-
-                <?php   if($vd?->getError('sandi')){ ?>
-                    <div class="alert alert-danger">
-                    <?php
-                         echo $vd?->getError('sandi');
-                    ?>
-                    </div>
-              <?php }  ?>
-            </div>
-
-            <button class="btn btn-primary mt-2">login</button>
-        </div>
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<div class="preloader">
+    <div class="loader">
+        <div class="loader__figure"></div>
+        <p class="loader__label">Elite admin</p>
     </div>
 </div>
-</form>
+<!-- ============================================================== -->
+<!-- Main wrapper - style you can find in pages.scss -->
+<!-- ============================================================== -->
+<section id="wrapper" class="login-register login-sidebar" style="background-image:url(<?= base_url('/assets/images/background/login-register.jpg') ?>);">
+    <div class="login-box card">
+        <div class="card-body">
+            <form class="form-horizontal form-material" id="loginform" method='POST' action="<?= base_url('/login') ?>">
+                <a href="javascript:void(0)" class="text-center db"><img src="<?= base_url('/assets/images/logo-icon.png') ?>" alt="Home" /><br /><img src="../assets/images/logo-text.png" alt="Home" /></a>
+                <div class="form-group m-t-40">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="text" name='email' required="" placeholder="Username">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="password" name='sandi' required="" placeholder="Password">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                            <label class="custom-control-label" for="customCheck1">Remember me</label>
+                            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group text-center m-t-20">
+                    <div class="col-xs-12">
+                        <button class="btn btn-info btn-lg btn-block text-uppercase btn-rounded" type="submit">Log In</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 m-t-10 text-center">
+                        <div class="social"><a href="javascript:void(0)" class="btn  btn-facebook" data-toggle="tooltip" title="Login with Facebook"> <i aria-hidden="true" class="fa fa-facebook"></i> </a> <a href="javascript:void(0)" class="btn btn-googleplus" data-toggle="tooltip" title="Login with Google"> <i aria-hidden="true" class="fa fa-google-plus"></i> </a> </div>
+                    </div>
+                </div>
+                <div class="form-group m-b-0">
+                    <div class="col-sm-12 text-center">
+                        Don't have an account? <a href="pages-register2.html" class="text-primary m-l-5"><b>Sign Up</b></a>
+                    </div>
+                </div>
+            </form>
+            <form class="form-horizontal" id="recoverform" action="index.html">
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <h3>Recover Password</h3>
+                        <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+                    </div>
+                </div>
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="text" required="" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group text-center m-t-20">
+                    <div class="col-xs-12">
+                        <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
 
-    <?=$this->endSection()?>
+<?= $this->endSection() ?>
